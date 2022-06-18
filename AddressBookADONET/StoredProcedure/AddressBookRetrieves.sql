dd7a63c5a389d80552ffef5bdaea2b313f1e263a
Shashank <langishashank@gmail.com>
2022-06-18 21:15:51 +0530

    [Shashank]: Implemented Retrieve data from AddressBook.

---
+++ AddressBookADONET/StoredProcedure/AddressBookRetrieves.sql
@@ -0,0 +1,44 @@
+use [AddressBookDetails]
+
+ALTER PROCEDURE AddressBookRetrieves
+@ID int
+
+AS
+SET XACT_ABORT ON;
+SET NOCOUNT ON;
+BEGIN
+BEGIN TRY
+BEGIN TRANSACTION;
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+
+DECLARE @result bit = 0;
+SELECT * FROM AddressDetails;
+COMMIT TRANSACTION;
+SET @result = 1;
+return @result;
+END TRY
+BEGIN CATCH
+
+IF(XACT_STATE()) = -1
+ BEGIN
+  PRINT
+   'transaction is uncommitable' + 'rolling back transaction'
+   ROLLBACK TRANSACTION;
+   return @result;
+ END;
+ELSE IF (XACT_STATE()) = 1
+ BEGIN
+  PRINT
+   'transaction is  commitable' + ' commiting back transaction'
+   COMMIT TRANSACTION;
+   SET @result =1;
+   return @result;
+ END;
+END CATCH
+END
+
+SELECT * FROM AddressDetails
+
+
